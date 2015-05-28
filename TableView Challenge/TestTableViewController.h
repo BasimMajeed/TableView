c36426d8b0db1f5fb61b4c2d78f4dc55501275db
BasimMajeed <basim.majeed@gmail.com>
2015-05-28 12:47:44 +0500

    Second commit

--- TableView Challenge/TestTableViewController.h
+++ TableView Challenge/TestTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  TestTableViewController.h
 //  TableView Challenge
 //
 //  Created by Basim Majeed on 28/05/2015.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TestTableViewController : UITableViewController
 
 @end
-
